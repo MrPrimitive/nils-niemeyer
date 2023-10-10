cec2b495abf9b12175f7d27dcafb74d662db0ba1
Nils Niemeyer <nils.niemeyer@hotmail.com>
2023-10-10 23:17:28 +0200

    new stuff

--- src/app/components/home/home-content/home-content-desktop.component.ts
+++ src/app/components/home/home-content/home-content-desktop.component.ts
@@ -3,33 +3,20 @@ import {Router} from '@angular/router'
 import {HomeContentHelper} from './home-content.helper'
 
 @Component({
-  selector: 'mse-home-content-desktop',
-  templateUrl: './templates/home-content-desktop.component.html',
-  styleUrls: ['./styles/home-content-desktop.component.scss']
+	selector: 'mse-home-content-desktop',
+	templateUrl: './templates/home-content-desktop.component.html',
+	styleUrls: ['./styles/home-content-desktop.component.scss']
 })
 export class HomeContentDesktopComponent extends HomeContentHelper implements OnInit, OnDestroy {
 
-  constructor(router: Router) {
-    super(router)
-  }
+	constructor(router: Router) {
+		super(router)
+	}
 
 
-  ngOnInit(): void {
-    window.addEventListener('scroll', this.scrollEvent);
-  }
+	ngOnInit(): void {
+	}
 
-  ngOnDestroy(): void {
-  }
-
-  private scrollEvent(event: any): void {
-    const screenHeight: number = window.screen.height;
-    const halfScreenHeight: number = screenHeight / 2
-    const quadScreenHeight: number = halfScreenHeight / 2
-
-    const mseHomeNameMyNameAnimationElement: HTMLElement | null = document.getElementById('content-dot-animation')
-    if (mseHomeNameMyNameAnimationElement) {
-      // const paddingTop: string = (40 - ((window.scrollY / halfScreenHeight) * 20)) + 'px';
-      mseHomeNameMyNameAnimationElement.style.paddingTop = ;
-    }
-  }
+	ngOnDestroy(): void {
+	}
 }
--- src/app/components/home/home-image-me/home-image-me-desktop.component.ts
+++ src/app/components/home/home-image-me/home-image-me-desktop.component.ts
@@ -1,11 +1,12 @@
 import {Component, OnDestroy, OnInit} from '@angular/core'
 import {Router} from '@angular/router'
 import {HomeImageMeHelper} from './home-image-me.helper'
+import {normalize} from '../../../_helper/function.util'
 
 @Component({
 	selector: 'mse-home-image-me-desktop',
 	templateUrl: './templates/home-image-me-desktop.component.html',
-	styleUrls: ['./styles/home-image-me.component.scss','./styles/home-image-me-desktop.component.scss']
+	styleUrls: ['./styles/home-image-me.component.scss', './styles/home-image-me-desktop.component.scss']
 })
 export class HomeImageMeDesktopComponent extends HomeImageMeHelper implements OnInit, OnDestroy {
 
@@ -15,8 +16,19 @@ export class HomeImageMeDesktopComponent extends HomeImageMeHelper implements On
 
 
 	ngOnInit(): void {
+		window.addEventListener('scroll', this.scrollEvent)
 	}
 
 	ngOnDestroy(): void {
 	}
+
+	private scrollEvent(event: any): void {
+		const imageElement: HTMLElement | null = document.getElementById('mse-home-background-image-animation')
+		if (imageElement) {
+			const rect: DOMRect = imageElement.getBoundingClientRect()
+			imageElement.style.opacity = String(1 - normalize(rect.top, 0, screen.height))
+		}
+	}
+
 }
+
