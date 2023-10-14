197a04d285d6ad5accbadb3c9ea84ce490bc1bd4
Nils Niemeyer <nils.niemeyer@hotmail.com>
2023-10-14 14:30:08 +0200

    clean up

--- src/app/components/home/home-content-experience/home-content-experience-small.component.ts
+++ src/app/components/home/home-content-experience/home-content-experience-small.component.ts
@@ -1,18 +1,18 @@
 import {Component, OnDestroy, OnInit} from '@angular/core'
 
 @Component({
-  selector: 'mse-home-content-experience-small',
-  templateUrl: './templates/home-content-experience.component.html',
-  styleUrls: ['./styles/home-content-experience-small.component.scss']
+	selector: 'mse-home-content-experience-small',
+	templateUrl: './templates/home-content-experience.component.html',
+	styleUrls: ['./styles/home-content-experience-small.component.scss']
 })
 export class HomeContentExperienceSmallComponent implements OnInit, OnDestroy {
-  constructor() {
-  }
+	constructor() {
+	}
 
-  ngOnDestroy(): void {
-  }
+	ngOnDestroy(): void {
+	}
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
 
 }
